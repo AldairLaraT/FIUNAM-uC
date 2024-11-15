85e588bda6e967105f1618105496f6bb084a0fd7
Aldair Lara <aldairlara@gmail.com>
2024-11-15 01:03:31 -0600

    Códigos de clase

--- Workspace/C27 - I2C_DS1307_int/main.c
+++ Workspace/C27 - I2C_DS1307_int/main.c
@@ -1,9 +1,36 @@
-
-
-/**
- * main.c
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * 
+ * School:      Universidad Nacional Autónoma de México | Facultad de Ingeniería
+ * Department:  Ingeniería Electrónica
+ * Subject:     Microprocesadores y Microcontroladores
+ * Topic:		Tema 09 | Periféricos
+ * Code:        Código 28 | Synchronous Serial Interface (SSI)
+ * Description: Uso del SSI_0 para controlar un potenciometro digital MCP41010.
+ * Author:      M. I. Christo Aldair Lara Tenorio
+ * Date:        16 de noviembre de 2024
+ * 
  */
-int main(void)
-{
-	return 0;
-}
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * Archivos de cabecera
+ */
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * Macros de apuntadores a registros
+ */
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * Macros de apoyo
+ */
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * Variables globales
+ */
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * Funciones
+ */
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
+ * Función principal
+ */
